61b29b2a328834623c502b98b97118553a5674d6
scottBowles <shbowles@gmail.com>
2020-10-07 00:06:47 -0400

    Memoize components in Calendar.js for better SectionList performance

--- src/screens/Calendar/Calendar.js
+++ src/screens/Calendar/Calendar.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { ActivityIndicator, SectionList, StyleSheet, View } from "react-native";
 
-import { dummyCalendarData, sectionizeCalendarData, useFetch } from "api";
+import { dummyCalendarData, sectionizeCalendarData } from "api";
 import { H1, H2, H3 } from "styles/typography";
-import { Loading, ColorBox } from "common/components";
+import { ColorBox } from "common/components";
 import {
   CalendarBlock,
   Content,
@@ -33,7 +33,7 @@ const styles = StyleSheet.create({
 const currentYear = new Date().getFullYear();
 // use to get current liturgical year's start year, for default start year
 
-const renderItem = ({ item: { commemorations, date, season } }) => (
+const Item = React.memo(({ item: { commemorations, date, season } }) => (
   <CalendarBlock date={date}>
     <DateBlock
       dateData={{ isFastDay: true }}
@@ -42,40 +42,41 @@ const renderItem = ({ item: { commemorations, date, season } }) => (
     />
     <Content season={season} commemorations={commemorations} />
   </CalendarBlock>
+));
+
+const SectionHeader = React.memo(
+  ({
+    section: {
+      section: { type, month, year, season },
+    },
+  }) => {
+    const seasonHeader = (
+      <View style={styles.seasonHeader} key={season + month + year}>
+        <ColorBox
+          color={season.colors[0]}
+          dimension={9}
+          style={styles.colorBox}
+        />
+        <H3>{season.name}</H3>
+      </View>
+    );
+    const monthHeader = (
+      <H3 key={month + year + season}>
+        {month} {year}
+      </H3>
+    );
+
+    return (
+      <View style={styles.container}>
+        {type === "both"
+          ? [seasonHeader, monthHeader]
+          : type === "month"
+          ? monthHeader
+          : seasonHeader}
+      </View>
+    );
+  }
 );
-
-const renderSectionHeader = ({
-  section: {
-    section: { type, month, year, season },
-  },
-}) => {
-  const seasonHeader = (
-    <View style={styles.seasonHeader} key={season + month + year}>
-      <ColorBox
-        color={season.colors[0]}
-        dimension={9}
-        style={styles.colorBox}
-      />
-      <H3>{season.name}</H3>
-    </View>
-  );
-  const monthHeader = (
-    <H3 key={month + year + season}>
-      {month} {year}
-    </H3>
-  );
-
-  return (
-    <View style={styles.container}>
-      {type === "both"
-        ? [seasonHeader, monthHeader]
-        : type === "month"
-        ? monthHeader
-        : seasonHeader}
-    </View>
-  );
-};
-
 const Calendar = ({ startYear = 2020 }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [dataSource, setDataSource] = useState([]);
@@ -103,7 +104,16 @@ const Calendar = ({ startYear = 2020 }) => {
   // const data = dummyCalendarData;
   // const isLoading = false;
 
-  const renderFooter = () => {
+  const ListHeader = React.memo(() => (
+    <View style={styles.container}>
+      <H1>The Church Year</H1>
+      <H2>
+        {startYear} - {+startYear + 1}
+      </H2>
+    </View>
+  ));
+
+  const ListFooter = React.memo(() => {
     return (
       // Footer View with Loader
       <View style={styles.footer}>
@@ -112,23 +122,17 @@ const Calendar = ({ startYear = 2020 }) => {
         ) : null}
       </View>
     );
-  };
+  });
 
   return (
     <SectionList
-      ListHeaderComponent={
-        <View style={styles.container}>
-          <H1>The Church Year</H1>
-          <H2>
-            {startYear} - {+startYear + 1}
-          </H2>
-        </View>
-      }
       sections={dataSource}
       keyExtractor={(item, index) => item + index}
-      renderItem={renderItem}
-      renderSectionHeader={renderSectionHeader}
-      ListFooterComponent={renderFooter}
+      ListHeaderComponent={ListHeader}
+      renderItem={({ item }) => <Item item={item} />}
+      renderSectionHeader={({ section }) => <SectionHeader section={section} />}
+      ListFooterComponent={ListFooter}
+      initialNumToRender={15}
       onEndReachedThreshold={15}
       onEndReached={getData}
     />
