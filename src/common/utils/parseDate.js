87e51ae4c7eb87ec1deec3711196b21860cc6294
scottBowles <shbowles@gmail.com>
2020-10-25 21:28:05 -0400

    Change parseDate util to use og date methods rather than UTC methods

--- src/common/utils/parseDate.js
+++ src/common/utils/parseDate.js
@@ -4,11 +4,12 @@ export const parseDate = (dateStr) => {
   const date = new Date(dateStr);
   if (!date) return { error: "Invalid date provided" };
 
-  const weekdayIndex = date.getUTCDay();
-  const monthIndex = date.getUTCMonth();
+  const weekdayIndex = date.getDay();
   const weekday = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"][
     weekdayIndex
   ];
+
+  const monthIndex = date.getMonth();
   const fullMonth = [
     "January",
     "February",
@@ -23,9 +24,10 @@ export const parseDate = (dateStr) => {
     "November",
     "December",
   ][monthIndex];
-  const dayOfMonth = date.getUTCDate();
+
+  const dayOfMonth = date.getDate();
   const month = fullMonth.slice(0, 3);
-  const year = date.getUTCFullYear();
+  const year = date.getFullYear();
 
   return { dayOfMonth, fullMonth, month, weekday, year };
 };
