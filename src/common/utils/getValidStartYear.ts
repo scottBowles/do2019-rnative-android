a386e1ac732869cfacfc79f5931384826acbbb3c
scottBowles <shbowles@gmail.com>
2020-11-15 15:50:22 -0500

    Make getValidStartYear get current liturgical year start year for default

--- src/common/utils/getValidStartYear.ts
+++ src/common/utils/getValidStartYear.ts
@@ -1,21 +1,17 @@
+import { getLiturgicalYear } from "./getLiturgicalYear";
+
 /**
  * Validate calendar start year -- if invalid, return current liturgical year
  * @param year - year to be validated
  * @return valid year
  */
-
 export const getValidStartYear = (year: number | string): number => {
-  // Validate calendar start year
-  // If invalid, return current liturgical year
-  const currentYear = new Date().getFullYear();
-  // TODO: use to get current liturgical year's start year, for default start year
-
   const yearInt = +year;
 
-  const startYear =
-    !Number.isNaN(yearInt) && 0 < yearInt && yearInt < 10000
-      ? yearInt
-      : currentYear;
+  if (!Number.isNaN(yearInt) && 0 < yearInt && yearInt < 10000) {
+    return yearInt;
+  }
 
-  return startYear;
+  const currentLiturgicalYear = getLiturgicalYear(new Date());
+  return currentLiturgicalYear;
 };
