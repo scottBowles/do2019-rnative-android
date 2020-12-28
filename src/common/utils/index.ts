12e02b9ddf6604515811a475df94bdadc14a7c19
scottBowles <shbowles@gmail.com>
2020-12-28 16:35:14 -0500

    Add getValidColor util

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -1,6 +1,7 @@
 export { getAdventOne } from "./getAdventOne";
 export { getLiturgicalYear } from "./getLiturgicalYear";
 export { getLocalDate } from "./getLocalDate";
+export { getValidColor } from "./getValidColor";
 export { getValidStartYear } from "./getValidStartYear";
 export { parseDate } from "./parseDate";
 export { toTitleCase } from "./toTitleCase";
