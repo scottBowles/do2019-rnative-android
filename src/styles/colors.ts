c5a6b51aad37a24308009bb2e372b61d1d7b1927
scottBowles <shbowles@gmail.com>
2020-12-28 16:35:32 -0500

    Add lenten array and blue colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,5 +1,6 @@
 export const colors = {
   black: "#000000",
+  blue: "#4169e1",
   fontGrey: "#333333",
   green: "#077339",
   lightGrey: "#d3d3d3",
@@ -8,4 +9,5 @@ export const colors = {
   red: "#c21c13",
   rose: "#ffc0cb",
   white: "#ffffff",
+  "lenten array": "#fce8e9",
 };
