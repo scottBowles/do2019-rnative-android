323857c2d20cb86cd5417adf5b4da8e5f23d3ff9
scottBowles <shbowles@gmail.com>
2020-12-28 00:08:29 -0500

    Convert CalendarBlock to styled component

--- src/common/components/calendarBlock/CalendarBlock.tsx
+++ src/common/components/calendarBlock/CalendarBlock.tsx
@@ -1,34 +1,18 @@
-import React from "react";
-import { StyleProp, View, ViewStyle } from "react-native";
+import styled from "styled-components/native";
 import { colors } from "styles/colors";
 
-interface Props {
+interface IProps {
   weekday: string;
   children: React.ReactNode;
 }
 
-// Expects DateBlock and Content children
-
-export const CalendarBlock: React.FC<Props> = ({
-  weekday,
-  children,
-  ...props
-}) => (
-  <View style={containerStyle(weekday)} {...props}>
-    {children}
-  </View>
-);
-
-const containerStyle = (weekday: string): StyleProp<ViewStyle> => {
-  return {
-    flex: 1,
-    marginVertical: 5,
-    marginHorizontal: 25,
-    padding: 6,
-    borderColor: "black",
-    borderWidth: 1,
-    borderRadius: 4,
-    backgroundColor:
-      weekday.toLowerCase() === "sun" ? colors.lightGrey : "#ffffff",
-  };
-};
+export const CalendarBlock = styled.View<IProps>`
+  flex: 1;
+  margin: 5px 25px;
+  padding: 6px;
+  border-color: black;
+  border-width: 1px;
+  border-radius: 4px;
+  background-color: ${(props) =>
+    props.weekday.toLowerCase() === "sunday" ? colors.lightGrey : "#ffffff"};
+`;
