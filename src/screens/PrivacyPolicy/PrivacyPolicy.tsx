ce3cbc76a282f5ba1bef6e0d801c49388c458e94
scottBowles <shbowles@gmail.com>
2020-12-28 13:30:41 -0500

    Remove superfluous container view by use of contentContainerStyle property

--- src/screens/PrivacyPolicy/PrivacyPolicy.tsx
+++ src/screens/PrivacyPolicy/PrivacyPolicy.tsx
@@ -1,28 +1,26 @@
 import { Footer } from "common/components";
 import React from "react";
-import { ScrollView, StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet } from "react-native";
 import { H1, H3, P, PLink, SmallItalics } from "styles/typography";
 
 import { PrivacyPolicyContent } from "./PrivacyPolicyContent";
 
 export const PrivacyPolicy: React.FC = () => (
-  <ScrollView>
-    <View style={styles.container}>
-      <PrivacyPolicyContent
-        Title={H1}
-        Heading={H3}
-        Para={P}
-        ParaItalic={SmallItalics}
-        ParaLink={PLink}
-      />
-      <Footer />
-    </View>
+  <ScrollView contentContainerStyle={styles.container}>
+    <PrivacyPolicyContent
+      Title={H1}
+      Heading={H3}
+      Para={P}
+      ParaItalic={SmallItalics}
+      ParaLink={PLink}
+    />
+    <Footer />
   </ScrollView>
 );
 
 const styles = StyleSheet.create({
   container: {
     alignItems: "center",
-    padding: 22.4,
+    margin: 22,
   },
 });
