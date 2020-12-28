2dd62bf67070ff037864e2b1e382cb84fe0cb7e0
scottBowles <shbowles@gmail.com>
2020-12-28 01:57:02 -0500

    Remove superfluous container view by use of contentContainerStyle property

--- src/screens/About/About.tsx
+++ src/screens/About/About.tsx
@@ -1,23 +1,21 @@
 import { Footer } from "common/components";
 import React from "react";
-import { ScrollView, StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet } from "react-native";
 import { Caption, H1, H3, P, PLink, SmallItalics } from "styles/typography";
 
 import { AboutContent } from "./AboutContent";
 
 export const About: React.FC = () => (
-  <ScrollView>
-    <View style={styles.container}>
-      <AboutContent
-        Title={H1}
-        Heading={H3}
-        Para={P}
-        ParaItalic={SmallItalics}
-        Caption={Caption}
-        ParaLink={PLink}
-      />
-      <Footer />
-    </View>
+  <ScrollView contentContainerStyle={styles.container}>
+    <AboutContent
+      Title={H1}
+      Heading={H3}
+      Para={P}
+      ParaItalic={SmallItalics}
+      Caption={Caption}
+      ParaLink={PLink}
+    />
+    <Footer />
   </ScrollView>
 );
 
