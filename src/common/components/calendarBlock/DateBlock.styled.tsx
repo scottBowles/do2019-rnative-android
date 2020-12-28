0a04ad1ba69d909b3546936ff40bcc59bb875bb7
scottBowles <shbowles@gmail.com>
2020-12-28 01:20:36 -0500

    Separate styling into styled file

---
+++ src/common/components/calendarBlock/DateBlock.styled.tsx
@@ -0,0 +1,71 @@
+import styled from "styled-components/native";
+import { colors } from "styles/colors";
+import { fonts } from "styles/fonts";
+import { Text } from "styles/typography";
+
+interface IContainerProps {
+  primaryColor: string;
+}
+interface IWrapperProps {
+  large?: boolean;
+}
+interface TextColor {
+  textColor: string;
+}
+
+const DateBlockContainer = styled.View<IContainerProps>`
+  background-color: ${(props) => colors[props.primaryColor]};
+  border-radius: 4px;
+  padding-top: 9px;
+  padding-bottom: 8px;
+  align-items: center;
+  border-color: black;
+  border-width: 1px;
+`;
+
+const TextWrapper = styled.View<IWrapperProps>`
+  height: ${(props) => (props.large ? "35.2px" : "17.6px")};
+`;
+
+const DateText = styled(Text)<TextColor>`
+  color: ${(props) => props.textColor};
+  text-transform: uppercase;
+  font-size: 16px;
+  bottom: 5px;
+  font-family: ${fonts.primary.semibold};
+`;
+
+const Weekday = styled(DateText)`
+  font-family: ${fonts.primary.bold};
+`;
+
+const DayOfMonth = styled(DateText)`
+  font-family: ${fonts.primary.semibold};
+  font-size: 32px;
+  bottom: 8px;
+`;
+
+const FastDisplayContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-top: 32px;
+`;
+
+const FastText = styled(Text)<TextColor>`
+  color: ${(props) => props.textColor};
+  text-transform: uppercase;
+  font-size: 16px;
+  font-family: ${fonts.primary.bold};
+  top: 3px;
+  margin-left: 4px;
+`;
+
+export {
+  DateBlockContainer,
+  DateText,
+  DayOfMonth,
+  FastDisplayContainer,
+  FastText,
+  TextWrapper,
+  Weekday,
+};
--- src/common/components/calendarBlock/DateBlock.tsx
+++ src/common/components/calendarBlock/DateBlock.tsx
@@ -6,22 +6,22 @@
  *   DateBlock (export)
  *   FastDisplay
  *
- * STYLES
- *   dateStyles
- *   getTextColor         Ensure contrast
- *   composeTextStyle     Based on textColor, per above
- *   composeBlockStyle    Based on primaryColor for the day
- *
  */
 
 import { CrossIcon } from "assets/icons";
 import { CalendarDay } from "data/calendarData/models";
 import React from "react";
-import { View } from "react-native";
-import styled from "styled-components/native";
 import { colors } from "styles/colors";
-import { fonts } from "styles/fonts";
-import { Text } from "styles/typography";
+
+import {
+  DateBlockContainer,
+  DateText,
+  DayOfMonth,
+  FastDisplayContainer,
+  FastText,
+  TextWrapper,
+  Weekday,
+} from "./DateBlock.styled";
 
 interface Props {
   day: CalendarDay;
@@ -33,18 +33,18 @@ export const DateBlock: React.FC<Props> = ({ day }) => {
 
   return (
     <DateBlockContainer primaryColor={primaryColor}>
-      <DateTextContainer>
+      <TextWrapper>
         <Weekday textColor={textColor}>{day.weekday.slice(0, 3)}</Weekday>
-      </DateTextContainer>
-      <DateTextContainer large>
+      </TextWrapper>
+      <TextWrapper large>
         <DayOfMonth textColor={textColor}>{day.dayOfMonth}</DayOfMonth>
-      </DateTextContainer>
-      <DateTextContainer>
+      </TextWrapper>
+      <TextWrapper>
         <DateText textColor={textColor}>{day.month}</DateText>
-      </DateTextContainer>
-      <DateTextContainer>
+      </TextWrapper>
+      <TextWrapper>
         <DateText textColor={textColor}>{day.year}</DateText>
-      </DateTextContainer>
+      </TextWrapper>
       {!!day.isFastDay && <FastDisplay textColor={textColor} />}
     </DateBlockContainer>
   );
@@ -56,50 +56,3 @@ const FastDisplay: React.FC<{ textColor: string }> = ({ textColor }) => (
     <FastText textColor={textColor}>Fast</FastText>
   </FastDisplayContainer>
 );
-
-const FastDisplayContainer = styled.View`
-  flex-direction: row;
-  align-items: center;
-  margin-top: 32px;
-`;
-
-const FastText = styled(Text)`
-  color: ${(props) => props.textColor};
-  text-transform: uppercase;
-  font-size: 16px;
-  font-family: ${fonts.primary.bold};
-  top: 3px;
-  margin-left: 4px;
-`;
-
-const DateText = styled(Text)`
-  color: ${(props) => props.textColor};
-  text-transform: uppercase;
-  font-size: 16px;
-  bottom: 5px;
-  font-family: ${fonts.primary.semibold};
-`;
-
-const Weekday = styled(DateText)`
-  font-family: ${fonts.primary.bold};
-`;
-
-const DayOfMonth = styled(DateText)`
-  font-family: ${fonts.primary.semibold};
-  font-size: 32px;
-  bottom: 8px;
-`;
-
-const DateTextContainer = styled.View`
-  height: ${(props) => (props.large ? "35.2px" : "17.6px")};
-`;
-
-const DateBlockContainer = styled.View`
-  background-color: ${(props) => colors[props.primaryColor]};
-  border-radius: 4px;
-  padding-top: 9px;
-  padding-bottom: 8px;
-  align-items: center;
-  border-color: black;
-  border-width: 1px;
-`;
