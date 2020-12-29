c32afd84b6313517f872cbf0977fcf4cf846aa43
scottBowles <shbowles@gmail.com>
2020-12-29 13:53:04 -0500

    Fix styling & convert to styled components

--- src/common/components/calendarBlock/ContentLine.tsx
+++ src/common/components/calendarBlock/ContentLine.tsx
@@ -3,6 +3,7 @@ import { ColorBox } from "common/components";
 import * as WebBrowser from "expo-web-browser";
 import React from "react";
 import styled, { css } from "styled-components/native";
+import { fonts } from "styles/fonts";
 import { Text } from "styles/typography";
 
 interface ContentLineProps {
@@ -11,13 +12,19 @@ interface ContentLineProps {
     colors: string[];
     name: string;
     links?: string[];
+    rank: { precedence: string };
   };
   style?: object;
 }
 
 export const ContentLine: React.FC<ContentLineProps> = ({
   type = "primary",
-  data: { colors, name, links },
+  data: {
+    colors,
+    name,
+    links,
+    rank: { precedence },
+  },
   style: incomingStyle,
   ...props
 }) => {
@@ -26,19 +33,22 @@ export const ContentLine: React.FC<ContentLineProps> = ({
    * the following is necessary. Each word is split into its own Text element.
    */
   const colorBoxes = colors.map((color, index) => (
-    <StyledColorBox key={index} color={color} type={type} />
+    <StyledColorBox
+      key={index}
+      color={color}
+      dimension={type === "primary" ? 11 : type === "secondary" ? 9 : 7}
+    />
   ));
 
   const words = name.split(" ").map((word, index, arr) => (
-    <StyledText key={word + index} type={type}>
+    <StyledText key={word + index} type={type} precedence={precedence}>
       {index !== arr.length - 1 ? word + " " : word}
     </StyledText>
   ));
 
   const linksDisplay = links
     ? links.map((link, index) => (
-        <Text key={link + index}>
-          {" "}
+        <Text key={link + index} style={{ paddingLeft: 5 }}>
           <ExternalLinkIcon
             size={9}
             color="black"
@@ -55,62 +65,44 @@ export const ContentLine: React.FC<ContentLineProps> = ({
   );
 };
 
-interface IContainerProps {
-  seasonLine?: boolean;
-}
-const Container = styled.View<IContainerProps>`
+const Container = styled.View`
   flex-direction: row;
-  align-items: flex-start;
-  padding-bottom: 4px;
+  align-content: center;
+  align-items: center;
   flex-wrap: wrap;
-  margin: ${(props) => (props.seasonLine ? "4px 0" : "0")};
+  margin: 4px 0 6px;
 `;
 
-interface ITyped {
-  type: string;
-}
-const StyledColorBox = styled(ColorBox)<ITyped>`
+const StyledColorBox = styled(ColorBox)`
   margin-right: 5px;
-  ${(props) =>
-    props.type === "primary" &&
-    css`
-      margin-top: 5px;
-      height: 11px;
-      width: 11px;
-    `}
-  ${(props) =>
-    props.type === "secondary" &&
-    css`
-      margin-top: 5px;
-      height: 9px;
-      width: 9px;
-    `}
-${(props) =>
-    props.type === "season" &&
-    css`
-      margin-top: 3px;
-      height: 7px;
-      width: 7px;
-    `}
 `;
 
-const StyledText = styled(Text)<ITyped>`
+/**
+ * Padding and negative margin present to account for Adobe Caslon Pro's
+ * significant bottom padding, which throws off alignment
+ */
+const StyledText = styled(Text)<{ type: string; precedence: string }>`
   ${(props) =>
     props.type === "primary" &&
     css`
+      font-family: ${(props) =>
+        +props.precedence < 4 ? fonts.primary.bold : fonts.primary.regular};
       font-size: 16px;
+      padding-top: 8px;
+      margin-bottom: -16px;
       letter-spacing: -0.5px;
     `}
   ${(props) =>
     props.type === "secondary" &&
     css`
-      font-size: 13px;
-      line-height: 19px;
+      font-size: 12.8px;
+      padding-top: 6.4px;
+      margin-bottom: -9.6px;
       letter-spacing: -0.5px;
     `}
 ${(props) =>
     props.type === "season" &&
     css`
-      font-size: 9px;
+      font-size: 9.6px;
     `}
 `;
