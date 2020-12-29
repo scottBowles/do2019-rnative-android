d2f7dfbecd04690c1392e6854b4d3d3ca62fa52e
scottBowles <shbowles@gmail.com>
2020-12-29 15:02:54 -0500

    Convert to styled components

--- src/common/components/Menu.tsx
+++ src/common/components/Menu.tsx
@@ -9,8 +9,8 @@
 
 import { CalendarIcon, ClockIcon, CogIcon, CrossIcon } from "assets/icons";
 import React from "react";
-import { StyleSheet, View } from "react-native";
 import { Link } from "react-router-native";
+import styled from "styled-components/native";
 import { Text } from "styles/typography";
 
 import { OutlineBtn } from "./OutlineBtn";
@@ -18,85 +18,69 @@ import { OutlineBtn } from "./OutlineBtn";
 export const Menu: React.FC = () => {
   const menuItems = [
     {
-      icon: <ClockIcon size={12} color="black" style={styles.icons} />,
+      icon: <ClockIcon size={12} color="black" style={{ marginRight: 2 }} />,
       text: "Now",
       route: "/office",
     },
     {
-      icon: <CalendarIcon size={12} color="black" style={styles.icons} />,
+      icon: <CalendarIcon size={12} color="black" style={{ marginRight: 2 }} />,
       text: "Calendar",
       route: "/calendar",
     },
     {
-      icon: <CogIcon size={12} color="black" style={styles.icons} />,
+      icon: <CogIcon size={12} color="black" style={{ marginRight: 2 }} />,
       text: "Settings",
       route: "/settings",
     },
     {
-      icon: <CrossIcon size={12} color="black" style={styles.icons} />,
+      icon: <CrossIcon size={12} color="black" style={{ marginRight: 2 }} />,
       text: "About",
       route: "/about",
     },
   ];
 
   return (
-    <View style={styles.menuContainer}>
+    <Container>
       {menuItems.map((item, index) => (
-        <Link
-          to={item.route}
-          underlayColor="#f0f4f7"
-          style={styles.linkItem}
-          key={index}
-        >
-          <OutlineBtn style={styles.outlineBtn}>
-            <View style={styles.btnContent}>
-              {item.icon}
-              <Text style={styles.textContainer}>
-                <Text style={styles.firstLetter}>{item.text[0]}</Text>
-                <Text style={styles.text}>{item.text.slice(1)}</Text>
-              </Text>
-            </View>
-          </OutlineBtn>
-        </Link>
+        <StyledLink to={item.route} underlayColor="#f0f4f7" key={index}>
+          <StyledOutlineBtn>
+            {item.icon}
+            <TextWrapper>
+              <FirstLetter>{item.text[0]}</FirstLetter>
+              <StyledText>{item.text.slice(1)}</StyledText>
+            </TextWrapper>
+          </StyledOutlineBtn>
+        </StyledLink>
       ))}
-    </View>
+    </Container>
   );
 };
 
-const styles = StyleSheet.create({
-  menuContainer: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    marginTop: 8,
-  },
-  linkItem: {
-    borderRadius: 11,
-  },
-  outlineBtn: {
-    flexDirection: "row",
-    padding: 6.6,
-    margin: 3.3,
-  },
-  btnContent: {
-    flexDirection: "row",
-    alignItems: "center",
-    height: 16,
-  },
-  textContainer: {
-    alignItems: "baseline",
-    top: 3,
-  },
-  icons: {
-    marginRight: 2,
-  },
-  firstLetter: {
-    fontSize: 16,
-    letterSpacing: 0.66,
-    textTransform: "uppercase",
-  },
-  text: {
-    fontSize: 11,
-    textTransform: "uppercase",
-    textAlignVertical: "center",
-  },
-});
+const Container = styled.View`
+  flex-direction: row;
+  justify-content: space-around;
+  margin-top: 8px;
+`;
+
+const StyledLink = styled(Link)`
+  border-radius: 11px;
+`;
+
+const StyledOutlineBtn = styled(OutlineBtn)`
+  height: 32px;
+`;
+
+const TextWrapper = styled(Text)`
+  align-items: baseline;
+  top: 3px;
+`;
+
+const StyledText = styled(Text)`
+  font-size: 11px;
+  text-transform: uppercase;
+`;
+
+const FirstLetter = styled(StyledText)`
+  font-size: 16px;
+  letter-spacing: 0.66px;
+`;
