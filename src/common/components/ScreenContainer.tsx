2322fc14bef053bba92d093afabc11c6117d33f4
scottBowles <shbowles@gmail.com>
2020-12-31 21:20:10 -0500

    Add ScreenContainer for reuse

---
+++ src/common/components/ScreenContainer.tsx
@@ -0,0 +1,6 @@
+import styled from "styled-components/native";
+
+export const ScreenContainer = styled.View`
+  align-items: center;
+  margin: 22px;
+`;
--- src/common/components/index.ts
+++ src/common/components/index.ts
@@ -1,5 +1,6 @@
 export { ColorBox } from "./ColorBox";
+export { Footer } from "./Footer";
 export { Loading } from "./Loading";
 export { Menu } from "./Menu";
 export { OutlineBtn } from "./OutlineBtn";
-export { Footer } from "./Footer";
+export { ScreenContainer } from "./ScreenContainer";
