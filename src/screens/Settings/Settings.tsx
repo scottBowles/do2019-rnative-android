3182a1eb5f646828deda00d4ef82f64e00eb9e06
scottBowles <shbowles@gmail.com>
2021-01-01 21:40:19 -0500

    Factor out top material

--- src/screens/Settings/Settings.tsx
+++ src/screens/Settings/Settings.tsx
@@ -5,8 +5,19 @@ import { H1, P, SmallItalics } from "styles/typography";
 import { MainSetting } from "./MainSetting";
 import { mainSettings } from "./mainSettings";
 
-export const Settings = () => (
-  <ScrollView contentContainerStyle={styles.container}>
+export const Settings = () => {
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <TopMaterial />
+      {mainSettings.map((mainSetting, index) => (
+        <MainSetting key={index} setting={mainSetting} />
+      ))}
+    </ScrollView>
+  );
+};
+
+const TopMaterial = () => (
+  <>
     <H1>Daily Office Settings</H1>
     <P>
       Your settings will be saved the next time you pray on the same
@@ -16,12 +27,7 @@ export const Settings = () => (
       Setting changes take effect immediately. You do not need to save settings
       after you have updated them.
     </SmallItalics>
-    {mainSettings.map((mainSetting, index) => (
-      <MainSetting key={index} setting={mainSetting} />
-    ))}
-
-    {/* <View style={{ height: 40, margin: 40 }} /> */}
-  </ScrollView>
+  </>
 );
 
 const styles = StyleSheet.create({
