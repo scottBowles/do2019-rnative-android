dc18d0120eaaf177205b68b0a2e78a14aa2f5b2b
scottBowles <shbowles@gmail.com>
2021-01-03 21:59:31 -0500

    Add default field for each setting and change `setting` field to `name`

--- src/data/settingsData/advancedSettings.ts
+++ src/data/settingsData/advancedSettings.ts
@@ -1,28 +1,32 @@
 export interface IAdvancedSetting {
-  setting: string;
+  name: string;
   storageKey: string;
+  default: string;
   options: string[];
   description: string;
 }
 
 export const advancedSettings = [
   {
-    setting: `VISUAL THEME`,
+    name: `VISUAL THEME`,
     storageKey: `visualTheme`,
+    default: `Use Computer Setting`,
     options: [`Use Computer Setting`, `Light Mode`, `Dark Mode`],
     description: `Use light mode or dark mode, or default to your computer's setting for light or dark mode`,
   },
 
   {
-    setting: `SUBSTITUTE SUNDAY/HOLY DAY (EUCHARISTIC) LECTIONARY`,
+    name: `SUBSTITUTE SUNDAY/HOLY DAY (EUCHARISTIC) LECTIONARY`,
     storageKey: `substituteLectionary`,
+    default: `Standard Daily Office`,
     options: [`Standard Daily Office`, `Sundays & Holy Days`],
     description: `On Sundays and major holy days, do you want to use the standard Daily Office readings or substitute the 3-year Sunday/Holy Day cycle? When the Daily Office is used for the principal service of a church, the prayer book instructs you to replace the Daily Office readings with the three year cycle of Sunday and Holy Day readings. This is generally done only in churches and not when using the Daily Office as a personal devotion at home.`,
   },
 
   {
-    setting: `CONFESSION INTRO LENGTH`,
+    name: `CONFESSION INTRO LENGTH`,
     storageKey: `confessionIntroLength`,
+    default: `Long Intro Only on Fast Days`,
     options: [
       `Long Intro Only on Fast Days`,
       `Short Intro Always`,
@@ -32,15 +36,17 @@ export const advancedSettings = [
   },
 
   {
-    setting: `ABSOLUTION STYLE`,
+    name: `ABSOLUTION STYLE`,
     storageKey: `absolutionStyle`,
+    default: `Deacon or Lay Person`,
     options: [`Deacon or Lay Person`, `Priest or Bishop`],
     description: `After the confession, read an absolution suitable for a priest or a prayer suitable for a deacon or lay person.`,
   },
 
   {
-    setting: `MORNING PRAYER INVITATORY`,
+    name: `MORNING PRAYER INVITATORY`,
     storageKey: `morningPrayerInvitatory`,
+    default: `Venite always`,
     options: [
       `Venite always`,
       `Celebratory Sundays and feasts`,
@@ -51,92 +57,105 @@ export const advancedSettings = [
   },
 
   {
-    setting: `READING HEADINGS`,
+    name: `READING HEADINGS`,
     storageKey: `readingHeadings`,
+    default: `Hide`,
     options: [`Hide`, `Show`],
     description: `Show or hide headings from the English Standard Version of the Bible in scripture readings`,
   },
 
   {
-    setting: `LANGUAGE STYLE FOR PRAYERS`,
+    name: `LANGUAGE STYLE FOR PRAYERS`,
     storageKey: `traditionalOrContemporary`,
+    default: `Traditional`,
     options: [`Traditional`, `Contemporary`],
     description: `Traditional and contemporary language options are available for the Kyrie (Lord have mercy) and the Lord's Prayer`,
   },
 
   {
-    setting: `NATIONAL HOLIDAY COLLECTS`,
+    name: `NATIONAL HOLIDAY COLLECTS`,
     storageKey: `nationalCollects`,
+    default: `All`,
     options: [`All`, `United States`, `Canada`, `None`],
     description: `Show country-specific commemorations for the United States, Canada, or both.`,
   },
 
   {
-    setting: `EVENING PRAYER SUFFRAGES`,
+    name: `EVENING PRAYER SUFFRAGES`,
     storageKey: `eveningPrayerSuffrages`,
+    default: `Rotating`,
     options: [`Rotating`, `Traditional`, `New`],
     description: `Choose which set of short prayers to be used each evening`,
   },
 
   {
-    setting: `ADDITIONAL COLLECTS`,
+    name: `ADDITIONAL COLLECTS`,
     storageKey: `additionalCollects`,
+    default: `Rotating`,
     options: [`Rotating`, `Fixed`],
     description: `Use a different collect for each day of the week, or use the same two collects (from the classic prayer books) each day.`,
   },
 
   {
-    setting: `PRAYERS IN THE TIME OF AN ELECTION`,
+    name: `PRAYERS IN THE TIME OF AN ELECTION`,
     storageKey: `electionPrayers`,
+    default: `On`,
     options: [`On`, `Off`],
     description: `Include election related collects until the U.S. General election. (Automatically disabled if only Canadian feasts are selected).`,
   },
 
   {
-    setting: `PRAYERS IN THE TIME OF PANDEMIC`,
+    name: `PRAYERS IN THE TIME OF PANDEMIC`,
     storageKey: `pandemicPrayers`,
+    default: `On`,
     options: [`On`, `Off`],
     description: `Include a rotating set of collects for the duration of this pandemic`,
   },
 
   {
-    setting: `GREAT LITANY AT MORNING PRAYER`,
+    name: `GREAT LITANY AT MORNING PRAYER`,
     storageKey: `greatLitanyMP`,
+    default: `Off`,
     options: [`Off`, `Wed, Fri, & Sun.`, `Everyday`],
     description: `Include the Great Litany after the collects at Morning Prayer`,
   },
 
   {
-    setting: `GREAT LITANY AT EVENING PRAYER`,
+    name: `GREAT LITANY AT EVENING PRAYER`,
     storageKey: `greatLitanyEP`,
+    default: `Off`,
     options: [`Off`, `Wed, Fri, & Sun.`, `Everyday`],
     description: `Include the Great Litany after the collects at Evening Prayer`,
   },
 
   {
-    setting: `GENERAL THANKSGIVING`,
+    name: `GENERAL THANKSGIVING`,
     storageKey: `generalThanksgiving`,
+    default: `On`,
     options: [`On`, `Off`],
     description: `Pray the General Thanksgiving at the end of Morning and Evening Prayer`,
   },
 
   {
-    setting: `PRAYER OF ST. JOHN CHRYSOSTOM`,
+    name: `PRAYER OF ST. JOHN CHRYSOSTOM`,
     storageKey: `prayerOfChrysostom`,
+    default: `On`,
     options: [`On`, `Off`],
     description: `Pray the Prayer of St. John Chrysostom at the end of Morning and Evening Prayer. This prayer is suitable when praying in a group.`,
   },
 
   {
-    setting: `THE GRACE`,
+    name: `THE GRACE`,
     storageKey: `rotatingOrTradGrace`,
+    default: `Rotating`,
     options: [`Rotating`, `Traditional`],
     description: `Rotate each day through the three provided conclusions, or always use the same one from the classic prayer books.`,
   },
 
   {
-    setting: `ADVENT "O" ANTIPHONS`,
+    name: `ADVENT "O" ANTIPHONS`,
     storageKey: `OAntiphons`,
+    default: `Literal`,
     options: [`Literal`, `Hymnal`, `Latin`, `None`],
     description: `The traditional "O" Antiphons are used before and after the first canticle in Evening Prayer during the last eight days of Advent. You can use literal translations of the original Latin, or the familiar paraphrases used in the hymn "O Come, O Come Emmanuel"`,
   },
