0a7b6c50a6fbe9efb17b7064cbf13f37ec80baef
scottBowles <shbowles@gmail.com>
2021-01-04 23:49:55 -0500

    Move, correct, and use IApiCalendarDay interface for typing

--- src/data/officeData/dummyOffice.ts
+++ src/data/officeData/dummyOffice.ts
@@ -1,7 +1,54 @@
-// url used: https://data.dailyoffice2019.com/api/v1/office/morning_prayer/2020-12-10?confession=long&absolution=priest
-// on 11/27/20
+// url used: https://data.dailyoffice2019.com/api/v1/office/morning_prayer/2020-12-10?confession=lonf&absolution=priest
+// on 1/4/21
 
-export const dummyOffice = {
+import { IApiCalendarDay } from "data/interfaces";
+
+interface IDummyOffice {
+  calendar_day: IApiCalendarDay;
+  modules: {
+    data: {
+      name: string;
+      lines: {
+        content: string;
+        line_type: string;
+        indented: boolean;
+      }[];
+    }[];
+  };
+}
+
+export const dummyOffice: IDummyOffice = {
+  calendar_day: {
+    date: "2020-12-10",
+    date_description: {
+      date: "2020-12-10",
+      weekday: "Thursday",
+      month: "10",
+      month_name: "December",
+      day: "10",
+      year: "2020",
+    },
+    season: { name: "Advent", colors: ["purple", "blue"] },
+    fast: { fast_day: 0, fast_day_description: "", fast_day_reason: [] },
+    commemorations: [
+      {
+        name: "Thursday after the Second Sunday in Advent",
+        rank: {
+          name: "ADVENT_FERIA",
+          formatted_name: "Advent Feria",
+          precedence: 6,
+        },
+        colors: ["purple", "blue"],
+        links: [],
+        collects: {
+          collect:
+            "Blessed Lord, who caused all Holy Scriptures to be written for our learning: Grant us so to hear them, read, mark, learn, and inwardly digest them, that by patience and the comfort of your holy Word we may embrace and ever hold fast the blessed hope of everlasting life, which you have given us in our Savior Jesus Christ; who lives and reigns with you and the Holy Spirit, one God, for ever and ever.  Amen.",
+          alternate_collect: null,
+          vigil_collect: null,
+        },
+      },
+    ],
+  },
   modules: {
     data: [
       {
@@ -18,149 +65,129 @@ export const dummyOffice = {
             line_type: "leader",
             indented: false,
           },
-          {
-            content: "ISAIAH 40:3",
-            line_type: "citation",
-            indented: false,
-          },
+          { content: "ISAIAH 40:3", line_type: "citation", indented: false },
         ],
       },
       {
         name: "Confession of Sin",
         lines: [
-          [
-            {
-              content: "Confession of Sin",
-              line_type: "heading",
-              indented: false,
-            },
-            {
-              content: "The Officiant says to the People",
-              line_type: "rubric",
-              indented: false,
-            },
-            {
-              content:
-                "Dearly beloved, the Scriptures teach us to acknowledge our many sins and offenses, not concealing them from our heavenly Father, but confessing them with humble and obedient hearts that we may obtain forgiveness by his infinite goodness and mercy. We ought at all times humbly to acknowledge our sins before Almighty God, but especially when we come together in his presence to give thanks for the great benefits we have received at his hands, to declare his most worthy praise, to hear his holy Word, and to ask, for ourselves and on behalf of others, those things which are necessary for our life and our salvation. Therefore, draw near with me to the throne of heavenly grace.",
-              line_type: "leader",
-              indented: false,
-            },
-            {
-              content: "Almighty and most merciful Father,",
-              line_type: "congregation",
-              indented: false,
-            },
-            {
-              content:
-                "we have erred and strayed from your ways like lost sheep.",
-              line_type: "congregation",
-              indented: true,
-            },
-            {
-              content: "We have followed too much the devices and desires",
-              line_type: "congregation",
-              indented: false,
-            },
-            {
-              content: "of our own hearts.",
-              line_type: "congregation",
-              indented: true,
-            },
-            {
-              content: "We have offended against your holy laws.",
-              line_type: "congregation",
-              indented: false,
-            },
-            {
-              content:
-                "We have left undone those things which we ought to have done,",
-              line_type: "congregation",
-              indented: false,
-            },
-            {
-              content: "and we have done those things which we ought not",
-              line_type: "congregation",
-              indented: true,
-            },
-            {
-              content: "to have done;",
-              line_type: "congregation",
-              indented: true,
-            },
-            {
-              content: "and apart from your grace, there is no health in us.",
-              line_type: "congregation",
-              indented: false,
-            },
-            {
-              content: "O Lord, have mercy upon us.",
-              line_type: "congregation",
-              indented: false,
-            },
-            {
-              content: "Spare all those who confess their faults.",
-              line_type: "congregation",
-              indented: false,
-            },
-            {
-              content:
-                "Restore all those who are penitent, according to your promises",
-              line_type: "congregation",
-              indented: false,
-            },
-            {
-              content: "declared to all people in Christ Jesus our Lord.",
-              line_type: "congregation",
-              indented: true,
-            },
-            {
-              content: "And grant, O most merciful Father, for his sake,",
-              line_type: "congregation",
-              indented: false,
-            },
-            {
-              content:
-                "that we may now live a godly, righteous, and sober life,",
-              line_type: "congregation",
-              indented: true,
-            },
-            {
-              content: "to the glory of your holy Name. Amen.",
-              line_type: "congregation",
-              indented: true,
-            },
-            {
-              content: "The Priest alone stands and says",
-              line_type: "rubric",
-              indented: false,
-            },
-            {
-              content:
-                "Almighty God, the Father of our Lord Jesus Christ, desires not the death of sinners, but that they may turn from their wickedness and live. He has empowered and commanded his ministers to pronounce to his people, being penitent, the absolution and remission of their sins. He pardons and absolves all who truly repent and genuinely believe his holy Gospel. For this reason, we beseech him to grant us true repentance and his Holy Spirit, that our present deeds may please him, the rest of our lives may be pure and holy, and that at the last we may come to his eternal joy; through Jesus Christ our Lord.",
-              line_type: "leader",
-              indented: false,
-            },
-            {
-              content: "Amen.",
-              line_type: "congregation",
-              indented: false,
-            },
-          ],
+          {
+            content: "Confession of Sin",
+            line_type: "heading",
+            indented: false,
+          },
+          {
+            content: "The Officiant says to the People",
+            line_type: "rubric",
+            indented: false,
+          },
+          {
+            content: "Let us humbly confess our sins to Almighty God.",
+            line_type: "leader",
+            indented: false,
+          },
+          {
+            content: "Almighty and most merciful Father,",
+            line_type: "congregation",
+            indented: false,
+          },
+          {
+            content:
+              "we have erred and strayed from your ways like lost sheep.",
+            line_type: "congregation",
+            indented: true,
+          },
+          {
+            content: "We have followed too much the devices and desires",
+            line_type: "congregation",
+            indented: false,
+          },
+          {
+            content: "of our own hearts.",
+            line_type: "congregation",
+            indented: true,
+          },
+          {
+            content: "We have offended against your holy laws.",
+            line_type: "congregation",
+            indented: false,
+          },
+          {
+            content:
+              "We have left undone those things which we ought to have done,",
+            line_type: "congregation",
+            indented: false,
+          },
+          {
+            content: "and we have done those things which we ought not",
+            line_type: "congregation",
+            indented: true,
+          },
+          {
+            content: "to have done;",
+            line_type: "congregation",
+            indented: true,
+          },
+          {
+            content: "and apart from your grace, there is no health in us.",
+            line_type: "congregation",
+            indented: false,
+          },
+          {
+            content: "O Lord, have mercy upon us.",
+            line_type: "congregation",
+            indented: false,
+          },
+          {
+            content: "Spare all those who confess their faults.",
+            line_type: "congregation",
+            indented: false,
+          },
+          {
+            content:
+              "Restore all those who are penitent, according to your promises",
+            line_type: "congregation",
+            indented: false,
+          },
+          {
+            content: "declared to all people in Christ Jesus our Lord.",
+            line_type: "congregation",
+            indented: true,
+          },
+          {
+            content: "And grant, O most merciful Father, for his sake,",
+            line_type: "congregation",
+            indented: false,
+          },
+          {
+            content: "that we may now live a godly, righteous, and sober life,",
+            line_type: "congregation",
+            indented: true,
+          },
+          {
+            content: "to the glory of your holy Name. Amen.",
+            line_type: "congregation",
+            indented: true,
+          },
+          {
+            content: "The Priest alone stands and says",
+            line_type: "rubric",
+            indented: false,
+          },
+          {
+            content:
+              "The Almighty and merciful Lord grant you absolution and remission of all your sins, true repentance, amendment of life, and the grace and consolation of his Holy Spirit.",
+            line_type: "leader",
+            indented: false,
+          },
+          { content: "Amen.", line_type: "congregation", indented: false },
         ],
       },
       {
         name: "Preces",
         lines: [
-          {
-            content: "The Preces",
-            line_type: "heading",
-            indented: false,
-          },
-          {
-            content: "All stand.",
-            line_type: "rubric",
-            indented: false,
-          },
+          { content: "The Preces", line_type: "heading", indented: false },
+          { content: "All stand.", line_type: "rubric", indented: false },
           {
             content: "O Lord, open our lips;",
             line_type: "leader",
@@ -193,11 +220,7 @@ export const dummyOffice = {
             line_type: "congregation",
             indented: false,
           },
-          {
-            content: "Praise the Lord.",
-            line_type: "leader",
-            indented: false,
-          },
+          { content: "Praise the Lord.", line_type: "leader", indented: false },
           {
             content: "The Lord’s Name be praised.",
             line_type: "congregation",
@@ -218,16 +241,8 @@ export const dummyOffice = {
             line_type: "congregation",
             indented: false,
           },
-          {
-            content: "Venite",
-            line_type: "heading",
-            indented: false,
-          },
-          {
-            content: "O Come",
-            line_type: "subheading",
-            indented: false,
-          },
+          { content: "Venite", line_type: "heading", indented: false },
+          { content: "O Come", line_type: "subheading", indented: false },
           {
             content: "Officiant and People, all standing",
             line_type: "rubric",
@@ -378,4 +393,3 @@ export const dummyOffice = {
     ],
   },
 };
-DJDT;
---
+++ src/data/officeData/index.ts
@@ -0,0 +1,1 @@
+export { dummyOffice } from "./dummyOffice";
