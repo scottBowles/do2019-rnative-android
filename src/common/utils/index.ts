24b3d37edd5a6d50ea94ebbb270c53cd431c1d54
scottBowles <shbowles@gmail.com>
2021-01-22 00:00:13 -0500

    Add isEmptyObject util

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -3,5 +3,6 @@ export { getLiturgicalYear } from "./getLiturgicalYear";
 export { getLocalDate } from "./getLocalDate";
 export { getValidColor } from "./getValidColor";
 export { getValidStartYear } from "./getValidStartYear";
+export { isEmptyObject } from "./isEmptyObject";
 export { parseDate } from "./parseDate";
 export { toTitleCase } from "./toTitleCase";
