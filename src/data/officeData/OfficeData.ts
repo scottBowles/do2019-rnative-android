d78fb593cedec4b80d1d86af834ac4c333218120
scottBowles <shbowles@gmail.com>
2021-01-25 22:25:27 -0500

    Amend to keep OfficeData class closer to the incoming api data shape

--- src/data/officeData/OfficeData.ts
+++ src/data/officeData/OfficeData.ts
@@ -3,10 +3,10 @@ import { CalendarDay } from "data/calendarData/models";
 import { IApiOfficeData } from "./IApiOfficeData";
 
 export class OfficeData extends CalendarDay {
-  data: IApiOfficeData["modules"]["data"];
+  modules: IApiOfficeData["modules"];
 
   constructor(apiOffice: IApiOfficeData) {
     super(apiOffice.calendar_day);
-    this.data = apiOffice.modules.data;
+    this.modules = apiOffice.modules;
   }
 }
