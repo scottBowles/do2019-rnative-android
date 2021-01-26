9f3aaa960d0665b00a7534351edd39b94d1c943e
scottBowles <shbowles@gmail.com>
2021-01-25 22:27:14 -0500

    Get office prayers rendering with appropriate components

--- src/screens/Office/Office.tsx
+++ src/screens/Office/Office.tsx
@@ -1,19 +1,3 @@
-/**
- *
- * IN THIS FILE
- *
- * parse dummyOffice data
- *
- * COMPONENTS
- *   Title
- *   Office (export default)
- *
- * Styles
- *
- */
-
-// TODO -- CONSIDER USING addIsFastDay API UTIL, IF IT PERFORMS BETTER THAN USING isFast HERE
-
 import { Loading } from "common/components";
 import {
   CalendarBlock,
@@ -24,20 +8,38 @@ import { isEmptyObject } from "common/utils";
 import { useDummyFetch } from "data/common";
 import { IApiOfficeData, OfficeData, dummyOffice } from "data/officeData";
 import React from "react";
-import { ScrollView, StyleSheet, View } from "react-native";
-import { H1 } from "styles/typography";
-
-import { Confession } from "./Confession";
-import { Invitatory } from "./Invitatory";
-import { OpeningSentence } from "./OpeningSentence";
+import { ScrollView, View } from "react-native";
+import styled from "styled-components";
+import { Caption, Congregation, H1, H2, P, Rubric } from "styles/typography";
 
 interface IOfficeProps {
   office: string;
   date: Date;
 }
+interface IOfficePrayers {
+  officeData: OfficeData;
+}
 
-const Title = ({ office }) => (
-  <H1 style={styles.title}>{`Daily\n${office} Prayer`}</H1>
+// Maps api line_type value to component for rendering
+const officeComponents = {
+  heading: H2,
+  leader: P,
+  congregation: Congregation,
+  citation: Caption,
+  rubric: Rubric,
+};
+
+const OfficePrayers: React.FC<IOfficePrayers> = ({ officeData }) => (
+  <>
+    {officeData.modules.data.map((section) => (
+      <SectionWrapper key={section.name}>
+        {section.lines.map(({ content, line_type, indented }) => {
+          const Line = officeComponents[line_type] || P;
+          return <Line indented={indented}>{content}</Line>;
+        })}
+      </SectionWrapper>
+    ))}
+  </>
 );
 
 export const Office: React.FC<IOfficeProps> = ({
@@ -45,52 +47,35 @@ export const Office: React.FC<IOfficeProps> = ({
   date = new Date(),
   // TODO: Need to handle date right with time zones. See getLocalDate comments.
 }) => {
-  // need to change this hook to make officeData `CalendarDay`s
-  // const [officeData, isLoading] = useFetch(
-  //   "https://data.dailyoffice2019.com/api/v1/calendar/2020-9-21?format=json"
-  // );
-
   const url = `https://data.dailyoffice2019.com/api/v1/office/morning_prayer/2020-12-10?confession=lonf&absolution=priest`;
   const [data, isLoading] = useDummyFetch(url, dummyOffice);
 
   if (isLoading || isEmptyObject(data)) {
-    return (
-      <View style={styles.container}>
-        <Loading />
-      </View>
-    );
+    return <Loading />;
   }
 
   const officeData = new OfficeData(data as IApiOfficeData);
 
   return (
-    <ScrollView>
-      <View style={styles.container}>
-        <Title office={office} />
-        <CalendarBlock weekday={officeData.weekday}>
-          <DateBlock day={officeData} />
-          <Content day={officeData} showSeason />
-        </CalendarBlock>
-        {/* <OpeningSentence
-          text={openingSentence.text}
-          citation={openingSentence.citation}
-        />
-        <Confession
-          office={officeData}
-          useLongFormInvitation={useLongFormInvitation}
-          useDeaconOrLayAbsolution={useDeaconOrLayAbsolution}
-        />
-        <Invitatory office={office} /> */}
-      </View>
-    </ScrollView>
+    <Container>
+      <Title>{`Daily\n${office} Prayer`}</Title>
+
+      <CalendarBlock weekday={officeData.weekday}>
+        <DateBlock day={officeData} />
+        <Content day={officeData} showSeason />
+      </CalendarBlock>
+
+      <OfficePrayers officeData={officeData} />
+    </Container>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    margin: 25,
-  },
-  title: {
-    marginBottom: 10,
-  },
-});
+const Container = styled(ScrollView)`
+  padding: 25px;
+`;
+
+const Title = styled(H1)`
+  margin-bottom: 10px;
+`;
+
+const SectionWrapper = styled(View)``;
