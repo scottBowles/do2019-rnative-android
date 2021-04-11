38ce33bd43cdc116ef99a81f7f751ba5d4ff91d8
scottBowles <shbowles@gmail.com>
2021-04-11 11:37:12 -0400

    Write parseLink util for share settings links

--- src/data/settingsData/utils.ts
+++ src/data/settingsData/utils.ts
@@ -10,11 +10,29 @@ const SETTINGS_BASE_URL = `https://www.dailyoffice2019.com/settings/`;
 
 export const getSettingsUrl = (settings: object) => {
   let url = SETTINGS_BASE_URL + "?";
-  // console.log({ settings });
+
   for (const setting of allSettings) {
     const value = settings[setting.name];
-    console.log({ value });
     if (value) url += `setting_${setting.name}=${value}&`;
   }
   return url;
+};
+
+export const parseLink = (link: string) => {
+  const queryString = link.split("?")[1];
+
+  const settingsUpdateObj = queryString
+    .split("&")
+    // remove the `setting_` portion of each string
+    .map((string) => string.substring(8))
+    // split keys and values
+    .map((text) => text.split("="))
+    // turn it into an object
+    .reduce((acc, cur) => {
+      const [key, value] = cur;
+      acc[key] = value;
+      return acc;
+    }, {});
+
+  return settingsUpdateObj;
 };
